b1542d5a344f731a317aaec7aba26080e8fda02d
llshl <lsh970708@naver.com>
2022-06-24 02:38:14 +0900

    베팅하기 함수 작성

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -22,12 +22,11 @@ contract Lottery {
 
     uint private _pot;
 
+    // 이벤트 로그들을 한번에 모을 수 있다, BET이라는 로그들을 찍어줘
+    // (몇번째 배팅인지, 누가 배팅했는지, 얼마 배팅했는지, 어떤글자로 베팅했는지, 어떤 블록에 정답이 있는지)
+    event BET(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
     constructor() public {
         owner = msg.sender; // msg.sender는 전역변수
-    }
-
-    function getSomeValue() public pure returns (uint256 value){
-        return 5;
     }
 
     function getPot() public view returns (uint256 value){ // 스마트 컨트랙의 변수를 가져와서 쓰려면 view 키ㅕ드를 쓴다
@@ -35,6 +34,24 @@ contract Lottery {
     }
 
     // Bet (베팅하기)
+    /*
+    @dev 베팅을 한다. 유저는 0.005ETH를 보내야하고 베팅용 1byte 글자를 보낸다, 큐에 저장된 베팅 정보는 이후 distribute 함수에서 해결한다
+    @param challenges 유저가 베팅하는 글자
+    @return 함수가 잘 수행되었는지 확인하는 bool 값
+    */
+    function bet(byte challenges) public payable returns (bool result){
+        // 돈이 제대로 왔는지 확인
+        // require는 if역할, msg.value는 컨트랙트가 받은금액, 문자열은 조건이 false때 출력할 문구
+        require(msg.value == BET_AMOUNT, "Not Enough ETH");
+
+        // 큐에 베팅정보를 넣기
+        require(pushBet(challenges), "Fail to add a new Bew Info");
+
+        // 이벤트 로그를 찍는다
+        // (몇번째 배팅인지, 누가 배팅했는지, 얼마 배팅했는지, 어떤글자로 베팅했는지, 어떤 블록에 정답이 있는지)
+        emit BET(_tail - 1, msg.sender, msg.value, challenges, block.number + BET_BLOCK_INTERVAL);
+    return true;
+}
     // 베팅한 값을 큐에 저장함
 
     // Distribute (검증하기)
@@ -50,7 +67,7 @@ contract Lottery {
     }
 
     // 큐 push
-    function pushBet(byte challenges) public returns (bool){
+    function pushBet(byte challenges) internal returns (bool){
         BetInfo memory b; // 베팅정보를 하나 생성하고 세팅한다
         b.bettor = msg.sender; // 함수 호출한 사람
         b.answerBlockNumber = block.number + BET_BLOCK_INTERVAL; // block.number는 현재 이 트랜잭션이 들어가게되는 블록넘버를 가져온다
@@ -63,7 +80,7 @@ contract Lottery {
     }
 
     // 큐 pop
-    function popBet(uint256 index) public returns (bool){
+    function popBet(uint256 index) internal returns (bool){
         // delete를 하면 가스를 돌려받는다. 왜? 상태데이터베이스에 저장된 값을 그냥 뽑아오겠다는 것이기에
         // 그러니 필요하지 않은 값이 있다면 delete를 해주자
         delete _bets[index];
